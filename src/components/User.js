eda00ca9ce83958813fa47ee781b766d3a2a6022
Restyled.io <commits@restyled.io>
2022-05-05 15:43:03 +0000

    Restyled by prettier

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,11 @@
 import React from "react";
-import {useEffect, useState} from "react";
-import {useMoralis} from "react-moralis";
-import {Card, Icon, Modal} from "web3uikit";
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+import { Card, Icon, Modal } from "web3uikit";
 
-function User({account}) {
-
+function User({ account }) {
   const [isVisible, setVisible] = useState(false);
-  const {Moralis} = useMoralis();
+  const { Moralis } = useMoralis();
   const [userRentals, setUserRentals] = useState();
 
   useEffect(() => {
@@ -20,11 +19,12 @@ function User({account}) {
     }
 
     fetchRentals();
-  }, [ isVisible ]);
+  }, [isVisible]);
 
   return (
-      <><div onClick = {() => setVisible(true)}>
-      <Icon fill = "#000000" size = {24} svg = "user" />
+    <>
+      <div onClick={() => setVisible(true)}>
+        <Icon fill="#000000" size={24} svg="user" />
       </div>
 
       <Modal
@@ -33,32 +33,34 @@ function User({account}) {
         title="Your Stays"
         isVisible={isVisible}
       >
-        <div style={{display:"flex", justifyContent:"start", flexWrap:"wrap", gap:"10px"}}>
-         {userRentals &&
-          userRentals.map((e)=>{
-            return(
-              <div style={{ width: "200px" }}>
-                <Card
-                  isDisabled
-                  title={e.attributes.city}
-                  description={`${e.attributes.datesBooked[0]} for ${e.attributes.datesBooked.length} Days`}
-                >
-                  <div>
-                    <img
-                      width="180px"
-                      src={e.attributes.imgUrl}
-                    />
-      </div>
-                </Card>
-      </div>
-            )
-          })
-
-         }
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "start",
+            flexWrap: "wrap",
+            gap: "10px",
+          }}
+        >
+          {userRentals &&
+            userRentals.map((e) => {
+              return (
+                <div style={{ width: "200px" }}>
+                  <Card
+                    isDisabled
+                    title={e.attributes.city}
+                    description={`${e.attributes.datesBooked[0]} for ${e.attributes.datesBooked.length} Days`}
+                  >
+                    <div>
+                      <img width="180px" src={e.attributes.imgUrl} />
+                    </div>
+                  </Card>
+                </div>
+              );
+            })}
         </div>
-
       </Modal>
-    </>);
+    </>
+  );
 }
 
 export default User;
